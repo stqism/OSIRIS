66b0917f8e04560c8d0d88c140c48f78df6efec2
Sean <sean@tox.im>
2014-09-30 02:22:10 -0700

    Uhhh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-cp osirisd $/bin/
+cp osirisd $1/bin/
 cp osiris.sh /etc/init.d/osiris
 cp osiris.conf $1/etc/osiris/
 cp -r testapp* $1/etc/osiris/app/
 mkdir -pf $1/etc/osiris/app
-chmod +x $/bin/osirisd
+chmod +x $1/bin/osirisd
 chmod +x /etc/init.d/osiris
 
-Please add the user osiris before running, settings for launching the daemon are in /etc/init.d/osiris
-You may need to change CONFIG=/usr/etc/osiris to $1/etc/osiris
+echo Please add the user osiris before running, settings for launching the daemon are in /etc/init.d/osiris
+echo You may need to change CONFIG=/usr/etc/osiris to $1/etc/osiris
